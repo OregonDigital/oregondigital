95ac87fefb3f224c147b5bfd115d076211d7585d
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-09-11 10:23:18 -0700

    introduce oregon_rdf_datastream

--- app/models/generic_asset.rb
+++ app/models/generic_asset.rb
@@ -2,4 +2,10 @@ class GenericAsset < ActiveFedora::Base
   include Hydra::ModelMixins::CommonMetadata
   include Hydra::ModelMethods
 
+  has_metadata :name => 'descMetadata', :type => OregonRDFDatastream
+  has_metadata :name => 'rightsMetadata', :type =>
+    Hydra::Datastream::RightsMetadata
+
+  has_file_datastream name: 'content'
+
 end
