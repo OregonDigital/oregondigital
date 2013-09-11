fda0856f93a18693cfbc6654ae733af2da70f3dc
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-11 11:22:03 -0700

    Updated the spec to make sure it can handle having a PID assigned.

--- spec/models/generic_asset_spec.rb
+++ spec/models/generic_asset_spec.rb
@@ -12,15 +12,27 @@ describe GenericAsset do
       end
     end
     context "when a new object is saved" do
-      before(:each) do
-        OregonDigital::IdService.should_receive(:mint).and_call_original
-        generic_asset.save
+      context "when it doesn't have a pid" do
+        before(:each) do
+          OregonDigital::IdService.should_receive(:mint).and_call_original
+          generic_asset.save
+        end
+        it "should no longer be a filler PID" do
+          expect(generic_asset.pid).not_to eq "__DO_NOT_USE__"
+        end
+        it "should be a valid NOID" do
+          expect(OregonDigital::IdService.valid?(generic_asset.pid)).to eq true
+        end
       end
-      it "should no longer be a filler PID" do
-        expect(generic_asset.pid).not_to eq "__DO_NOT_USE__"
-      end
-      it "should be a valid NOID" do
-        expect(OregonDigital::IdService.valid?(generic_asset.pid)).to eq true
+      context "but it already has a pid" do
+        subject(:generic_asset) {GenericAsset.new(:pid => "changeme:monkeys")}
+        before(:each) do
+          generic_asset.save
+        end
+        it "should not override the pid" do
+          expect(generic_asset.pid).to eq "changeme:monkeys"
+          expect(generic_asset).to be_persisted
+        end
       end
     end
   end
