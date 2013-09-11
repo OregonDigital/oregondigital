c29d5a942a4a9331f691dfc9427f37bfd9421c2e
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-11 14:02:40 -0700

    Added thumbnail derivative generation.

---
+++ app/models/image.rb
@@ -0,0 +1,9 @@
+class Image < GenericAsset
+  include Hydra::Derivatives
+  has_file_datastream name: "thumbnail"
+
+  makes_derivatives do |obj|
+    obj.transform_datastream :content, {:thumb => {:size => "120x120>", :datastream => "thumbnail"}}
+  end
+
+end
