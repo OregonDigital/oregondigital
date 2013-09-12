955d729e8760a2c79d5342afe3bdc9bfdd3cc870
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-12 13:36:32 -0700

    Added pyramidal tiff generation.

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
+require 'filemagic'
 
 describe Image do
-  subject(:image) {Image.new}
+  subject(:image) { Image.new }
   describe 'datastreams' do
     it 'should have a content datastream' do
       expect(image.content).to be_kind_of(ActiveFedora::Datastream)
@@ -23,15 +24,21 @@ describe Image do
     end
     context 'when the image has content' do
       before(:each) do
-        file = File.open(File.join(fixture_path, 'hydra.png'))
+        file = File.open(File.join(fixture_path, 'fixture_image.jpg'))
         image.add_file_datastream(file, :dsid => 'content')
         expect(image.content).not_to be_blank
         image.save
-        expect(image.thumbnail.content).to be_blank
         image.create_derivatives
       end
       it 'should populate the thumbnail datastream' do
-        expect(image.thumbnail.content).not_to be_blank
+        expect(image.thumbnail.content).not_to be_nil
+        expect(image.thumbnail.content).not_to eq ''
+      end
+      it 'should populate the pyramidal datastream' do
+        expect(image.pyramidal.content).not_to be_nil
+        expect(image.pyramidal.content).not_to eq ''
+        mime_type = FileMagic.new(FileMagic::MAGIC_MIME).buffer(image.pyramidal.content).split(';')[0]
+        expect(mime_type).to eq 'image/tiff'
       end
     end
   end
