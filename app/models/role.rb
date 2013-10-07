2d67c39b439c56d77cefe33bf5dd8a56079bd689
Trey Terrell <trey.terrell@oregonstate.edu>
2013-10-07 10:49:29 -0700

    Added frontend for managing IP Based Groups.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,12 @@
+# This is pulled from the Hydra-Role-Management gem and modified. Need to update upstream to use a module
+# to make this easily extendable.
 class Role < ActiveRecord::Base
+  has_and_belongs_to_many :users
   has_many :ip_ranges
+
+  validates :name,
+            uniqueness: true,
+            format: { with: /\A[a-zA-Z0-9._-]+\z/,
+                      :message => "Only letters, numbers, hyphens, underscores and periods are allowed"}
+
 end
