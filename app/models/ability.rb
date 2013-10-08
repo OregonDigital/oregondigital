0abfc66c66b53d53b721b2b5b33bf778a48e6e84
Trey Terrell <trey.terrell@oregonstate.edu>
2013-10-07 17:00:26 -0700

    Improved show/edit form, updated the ip ranges spec to show more features, and improved admin's abilities in regards to roles.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
   self.ability_logic += [:role_permissions]
   def role_permissions
     if current_user.admin?
-      can [:create, :show, :add_user, :remove_user, :index, :add_ip_range, :remove_ip_range], Role
+      can [:create, :show, :edit, :destroy, :add_user, :remove_user, :index, :add_ip_range, :remove_ip_range], Role
     end
   end
 end
--- spec/features/ip_ranges_spec.rb
+++ spec/features/ip_ranges_spec.rb
@@ -14,43 +14,60 @@ describe "IP Range Administration" do
     r.save
     r
   end
-  context "when a user is logged in" do
-    before(:each) do
-      visit root_path
-      capybara_login(user)
-    end
-    context "and they are a user" do
-      it "should not let them to the role admin page" do
-        expect{visit role_management.role_path(role)}.to raise_error(CanCan::AccessDenied)
-      end
-    end
-    context "and they are an admin" do
+  describe "edit role" do
+    context "when a user is logged in" do
       before(:each) do
-        user.roles << role
+        visit root_path
+        capybara_login(user)
       end
-      context "and they try to add IP ranges to a role" do
+      context "and they are a user" do
+        it "should not let them to the role admin page" do
+          expect{visit role_management.edit_role_path(role)}.to raise_error(CanCan::AccessDenied)
+        end
+      end
+      context "and they are an admin" do
         before(:each) do
-          visit role_management.role_path(role)
-          within("#ip-ranges") do
-            fill_in :ip_range_start, :with => "127.0.0.1"
-            fill_in :ip_range_end, :with => "127.0.0.1"
-            click_button "Add"
-          end
+          user.roles << role
+          user.save
         end
-        it "should work" do
-          within("#ip-ranges") do
-            expect(page).to have_content("127.0.0.1 - 127.0.0.1")
-          end
-        end
-        context "and they try to delete the IP range they added" do
+        context "and they try to delete the role" do
           before(:each) do
+            new_role = Role.new
+            new_role.name = "test"
+            new_role.save
+            visit role_management.edit_role_path(new_role)
+            within('.edit_role') do
+              click_link "Delete"
+            end
+          end
+          it "should delete the role" do
+            expect(page).to have_content("Role was successfully deleted.")
+          end
+        end
+        context "and they try to add IP ranges to a role" do
+          before(:each) do
+            visit role_management.edit_role_path(role)
             within("#ip-ranges") do
-              click_button("Remove Range")
+              fill_in :ip_range_start, :with => "127.0.0.1"
+              fill_in :ip_range_end, :with => "127.0.0.1"
+              click_button "Add"
             end
           end
           it "should work" do
             within("#ip-ranges") do
-              expect(page).not_to have_content("127.0.0.1 - 127.0.0.1")
+              expect(page).to have_content("127.0.0.1 - 127.0.0.1")
+            end
+          end
+          context "and they try to delete the IP range they added" do
+            before(:each) do
+              within("#ip-ranges") do
+                click_button("Remove Range")
+              end
+            end
+            it "should work" do
+              within("#ip-ranges") do
+                expect(page).not_to have_content("127.0.0.1 - 127.0.0.1")
+              end
             end
           end
         end
