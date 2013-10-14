c0c1d39dffdcc9e57ec7899ea1a3867d421db625
Jeremy Echols <jechols@uoregon.edu>
2013-10-14 15:56:54 -0700

    Refactor specs to use factories

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -1,14 +1,10 @@
 require 'spec_helper'
 
 describe "item review behavior" do
-  let (:item) {GenericAsset.new}
   context "with an unreviewed item" do
-    before(:each) do
-      item.read_groups = ["public"]
-      item.save
-    end
     context "on the catalog page" do
       before(:each) do
+        FactoryGirl.create(:generic_asset, :pending_review)
         visit catalog_index_path(:search_field => 'all_fields')
       end
       it "should not show the item" do
@@ -17,13 +13,9 @@ describe "item review behavior" do
     end
   end
   context "with a reviewed item" do
-    before(:each) do
-      item.read_groups = ["public"]
-      item.review!
-      expect(item).to be_persisted
-    end
     context "on the catalog page" do
       before(:each) do
+        FactoryGirl.create(:generic_asset)
         visit catalog_index_path(:search_field => 'all_fields')
       end
       it "should show the item" do
