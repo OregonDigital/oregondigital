8bdef1f4f756617b837c311aa04ae8395bc82b59
Trey Terrell <trey.terrell@oregonstate.edu>
2013-11-22 15:04:38 -0800

    Added more tests.

--- spec/models/datastream/oregon_resource_spec.rb
+++ spec/models/datastream/oregon_resource_spec.rb
@@ -17,16 +17,38 @@ describe Datastream::OregonResource do
     it "should let you access the attribute" do
       expect(subject.descMetadata.title).to eq ["bla"]
     end
+    it "should mark it as changed" do
+      expect(subject.descMetadata).to be_changed
+    end
     context "after it is persisted" do
       before(:each) do
         subject.save
         subject.reload
       end
-      it "should be accessible after being saved" do
-        expect(subject.descMetadata.title).to eq ["bla"]
+      context "and it's reloaded" do
+        before(:each) do
+          subject.reload
+        end
+        it "should be accessible after being saved" do
+          expect(subject.descMetadata.title).to eq ["bla"]
+        end
+        it "should serialize to content" do
+          expect(subject.descMetadata.content).not_to be_blank
+        end
       end
-      it "should serialize to content" do
-        expect(subject.descMetadata.title).not_to be_blank
+      context "and it is found again" do
+        before(:each) do
+          @object = DummyAsset.find(subject.pid)
+        end
+        it "should serialize to content" do
+          expect(@object.descMetadata.content).not_to be_blank
+        end
+        it "should be accessible after being saved" do
+          expect(@object.descMetadata.title).to eq ["bla"]
+        end
+        it "should have datastream content" do
+          expect(@object.descMetadata.datastream_content).not_to be_blank
+        end
       end
     end
   end
