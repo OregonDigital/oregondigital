5d861310e0636739425331ff733f14f64828097b
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-12-05 21:15:25 -0800

    use temporary repositiories; temporarily

--- lib/oregon_digital/rdf/rdf_types/license.rb
+++ lib/oregon_digital/rdf/rdf_types/license.rb
@@ -1,6 +1,7 @@
 module OregonDigital::RDF
   module RdfTypes
     class License < RdfResource
+      configure :repository => :default
       property :title, :predicate => RDF::DC.title, :class_name => RDF::Literal do |index|
         index.as :displayable, :facetable
       end
