f114ae35fcb9ea6847670ec3856a19e693e899f9
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-12-10 17:26:55 -0800

    add module level comment for RdfRepositories

--- lib/oregon_digital/rdf/rdf_repositories.rb
+++ lib/oregon_digital/rdf/rdf_repositories.rb
@@ -1,4 +1,19 @@
 module OregonDigital::RDF
+  ##
+  # Defines module methods for registering an RDF::Repository for
+  # persistence of RdfResources.
+  #
+  # This allows any triplestore (or other storage platform) with an
+  # RDF::Repository implementation to be used for persistence of
+  # resources that will be shared between ActiveFedora::Base objects.
+  #
+  #    RdfRepositories.add_repository :blah, RDF::Repository.new
+  #
+  # Multiple repositories can be registered to keep different kinds of
+  # resources seperate. This is configurable on subclasses of
+  # RdfResource at the class level.
+  #
+  # @see RdfConfigurable
   module RdfRepositories
 
     def add_repository(name, repo)
