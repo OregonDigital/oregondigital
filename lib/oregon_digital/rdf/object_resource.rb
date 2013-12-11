ae7af92584473d698e2b93338a4f312ea2416603
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-12-10 17:29:42 -0800

    add some additional top-level class/module comments

--- lib/oregon_digital/rdf/object_resource.rb
+++ lib/oregon_digital/rdf/object_resource.rb
@@ -1,4 +1,9 @@
 module OregonDigital::RDF
+  ##
+  # A class of RdfResources to act as the primary/root resource associated
+  # with a Datastream and ActiveFedora::Base object.
+  #
+  # @see OregonDigital::RdfResourceDatastream
   class ObjectResource < RdfResource
     configure :base_uri => "http://oregondigital.org/resource/"
   end
