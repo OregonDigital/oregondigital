f22c53d1d7ecc7f56782df8c66a84a48f80ba354
Jeremy Echols <jechols@uoregon.edu>
2013-12-16 12:21:34 -0800

    Add hard-coded attributes map

---
+++ config/initializers/ingest_form_map.rb
@@ -0,0 +1,51 @@
+# TODO: Pull this data from an actual dynamic map of some sort!
+INGEST_MAP = {
+  title: {
+    title: :title,
+  },
+
+  subject: {
+    subject: :subject,
+  },
+
+  type: {
+    type: :type,
+  },
+
+  format: {
+    hasFormat: :hasFormat,
+  },
+
+  date: {
+    created: :created,
+    modified: :modified,
+    date: :date,
+  },
+
+  description: {
+    description: :description
+  },
+
+  grouping: {
+    set: :set
+  },
+
+  identifier: {
+    identifier: :identifier,
+  },
+
+  right: {
+    rights: :rights
+  },
+
+  publisher: {
+  },
+
+  administrative: {
+  },
+
+  geographic: {
+    location: :location,
+  },
+}
+Metadata::Ingest::Form.internal_groups = INGEST_MAP.keys.collect {|key| key.to_s.pluralize}
