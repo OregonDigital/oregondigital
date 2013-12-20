cad362ac0b11603e323686e66e772d06d345443d
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-12-20 14:36:18 -0800

    put repository configuration in to_prepare block to fix reload! problem

--- config/initializers/rdf_repositories.rb
+++ config/initializers/rdf_repositories.rb
@@ -1,2 +1,10 @@
-OregonDigital::RDF::RdfRepositories.add_repository :default, RDF::Redis.new
-OregonDigital::RDF::RdfRepositories.add_repository :vocabs, RDF::Redis.new(:name => :vocabs)
+
+Rails.application.config.to_prepare do
+  if Rails.env.test?
+    OregonDigital::RDF::RdfRepositories.add_repository :default, RDF::Repository.new
+    OregonDigital::RDF::RdfRepositories.add_repository :vocabs, RDF::Repository.new
+  else
+    OregonDigital::RDF::RdfRepositories.add_repository :default, RDF::Redis.new
+    OregonDigital::RDF::RdfRepositories.add_repository :vocabs, RDF::Redis.new(:name => :vocabs)
+  end
+end
