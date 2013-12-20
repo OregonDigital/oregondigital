66dd08eea1c252f76d41c67837a41f28b464e304
Trey Terrell <trey.terrell@oregonstate.edu>
2013-12-20 09:43:25 -0800

    Added set syncing.

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,13 +18,14 @@ set :deploy_via, :remote_cache
 set :deploy_to, config['deploy_to']
 set :use_sudo, false
 set :keep_releases, 5
-set :shared_children, shared_children + %w{pids sockets tmp media}
+set :shared_children, shared_children + %w{pids sockets tmp media set_content}
 
 # if you want to clean up old releases on each deploy uncomment this:
 after 'deploy:restart', 'deploy:cleanup'
 
 after 'deploy:finalize_update', 'deploy:symlink_config'
 after 'deploy:update_code', 'deploy:migrate'
+before 'deploy:restart', 'sets:sync'
 after 'deploy:restart', 'deploy:cleanup'
 
 namespace :deploy do
@@ -33,5 +34,13 @@ namespace :deploy do
     %w{app.yml unicorn.rb god.conf}.each do |config_file|
       run "ln -nfs #{deploy_to}/shared/config/#{config_file} #{release_path}/config/#{config_file}"
     end
+  end
+end
+namespace :sets do
+  desc "Updates Collection Pages"
+  task :sync, :roles => :app do
+    rake = fetch(:rake, "rake")
+    rails_env = fetch(:rails_env, "production")
+    run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} sets:content:sync"
   end
 end
