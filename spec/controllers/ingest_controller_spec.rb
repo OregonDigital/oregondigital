b4f6d4ff05a6d54b12880de46d2c4d188bf01caf
Jeremy Echols <jechols@uoregon.edu>
2013-12-28 16:33:13 -0800

    Implement ingest form save for new objects

--- spec/controllers/ingest_controller_spec.rb
+++ spec/controllers/ingest_controller_spec.rb
@@ -44,4 +44,50 @@ describe IngestController do
       get :form
     end
   end
+
+  describe "#save" do
+    before(:each) do
+      # Fake asset to avoid AF hits, create expectations, etc
+      @asset = double("GenericAsset instance").as_null_object
+
+      # This is ugly but it mimics exactly what Rails params look like
+      @attrs = {
+        "titles_attributes" => {
+          "0" => {"type" => "title", "value" => "test title", "_destroy" => "false"}
+        },
+        "subjects_attributes" => {
+          "0" => {"type" => "subject", "value" => "foo", "_destroy" => "false"},
+          "1" => {"type" => "subject", "value" => "bar", "_destroy" => "false"}
+        }
+      }
+    end
+
+    context "(when the form represents a new asset)" do
+      before(:each) do
+        GenericAsset.stub(:new => @asset)
+      end
+
+      it "should set single-value attributes to a scalar value" do
+        expect(@asset).to receive(:title=).with("test title")
+        post :save, :metadata_ingest_form => @attrs
+      end
+
+      it "should set multiple-value attributes to arrays" do
+        expect(@asset).to receive(:subject=).with(["foo", "bar"])
+        post :save, :metadata_ingest_form => @attrs
+      end
+
+      it "shouldn't set anything on empty attributes" do
+        @attrs["titles_attributes"]["0"]["type"] = ""
+        @attrs["titles_attributes"]["0"]["value"] = ""
+        expect(@asset).not_to receive(:title=)
+        post :save, :metadata_ingest_form => @attrs
+      end
+
+      it "should create a new asset" do
+        @asset.should_receive(:save).once
+        post :save, :metadata_ingest_form => @attrs
+      end
+    end
+  end
 end
