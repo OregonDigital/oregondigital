79b74797313f79060dcd7358d0abe88e6d601202
Jeremy Echols <jechols@uoregon.edu>
2013-12-28 16:33:13 -0800

    Implement ingest form save for new objects

--- app/controllers/ingest_controller.rb
+++ app/controllers/ingest_controller.rb
@@ -1,7 +1,10 @@
+require 'metadata/ingest/translators/form_to_attributes'
+
 class IngestController < ApplicationController
   def index
   end
 
+  # Combined new/edit form handler
   def form
     @form = Metadata::Ingest::Form.new
 
@@ -10,5 +13,16 @@ class IngestController < ApplicationController
         @form.send("build_#{group}")
       end
     end
+  end
+
+  # Combined create/update form handler
+  def save
+    @form = Metadata::Ingest::Form.new(params[:metadata_ingest_form].to_hash)
+    @asset = GenericAsset.new
+    Metadata::Ingest::Translators::FormToAttributes.map = INGEST_MAP
+    Metadata::Ingest::Translators::FormToAttributes.from(@form).to(@asset)
+    @asset.save
+
+    redirect_to :ingest
   end
 end
