c94fea13faadf7481ec8e8a88bb0a96617e39788
Jeremy Echols <jechols@uoregon.edu>
2014-01-05 15:24:15 -0800

    Implement dynamic vocab map builder
    Builds the @controlled_vocab_map data structure based on the ingest
    map's defined attributes and the QA-like classes defined on the
    OregonRDF datastream

--- app/controllers/ingest_controller.rb
+++ app/controllers/ingest_controller.rb
@@ -43,12 +43,38 @@ class IngestController < ApplicationController
     Metadata::Ingest::Translators::FormToAttributes.map = ingest_map
   end
 
+  # Iterates over the ingest map, and looks up properties in the datastream
+  # definition to see where we need controlled vocab and what the URL will be
   def build_controlled_vocabulary_map
-    # TODO: Replace this with something that dynamically sets up controlled vocabulary data
-    # based on the form map and the datastream's properties
-    @controlled_vocab_map = {
-      "subject" => {
-        "subject" => qa.search_path(:vocab => "Subject::QaLcsh", :q => "VOCABQUERY")
-      }
-    }
+    @controlled_vocab_map = {}
+
+    for group, type_map in ingest_map
+      @controlled_vocab_map[group.to_s] = {}
+      for type, attribute in type_map
+        property = Datastream::OregonRDF.properties[attribute]
+
+        # TODO: What does it mean if we have no property for a mapped
+        # attribute?  Likely a misconfiguration that the user cannot control.
+        # We should send an error report somewhere here and silently move on,
+        # as a browser error isn't going to be particularly useful :-/
+        unless property
+          raise "Invalid attribute while building controlled vocabulary map: group => " +
+                "#{group.inspect}, type => #{type.inspect}, attribute => #{attribute.inspect}"
+        end
+
+        # Without a class, the data is simple and not something we try to
+        # translate in any way
+        next unless property[:class_name]
+
+        # This is ugly - maybe we should have something more directly exposing the QA-friendly vocab parameter
+        # (or create a map of param-to-class that each controlled vocab class registers and exposes)
+        qa_class = property[:class_name].qa_interface.class
+        vocab_param = qa_class.to_s.sub(/^OregonDigital::ControlledVocabularies::/, "")
+        next if vocab_param.blank?
+
+        path = qa.search_path(:vocab => vocab_param, :q => "VOCABQUERY")
+        @controlled_vocab_map[group.to_s][type.to_s] = path
+      end
+    end
+  end
 end
