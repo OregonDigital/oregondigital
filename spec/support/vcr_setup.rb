1e6ac173d49f30600ba67aea9b0da02e08604574
Jeremy Echols <jechols@uoregon.edu>
2014-01-09 04:57:57 -0800

    Use JSON serialization for VCR
    For some reason, this fixes an issue with YML cassettes always being
    encoded in base 64, which resulted in unreadable (and larger) files

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -2,4 +2,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = true
+  c.default_cassette_options = {serialize_with: :json}
 end
