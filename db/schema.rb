51d6552ec1289f26eda9ba15f06c0c8b35465bd3
Jeremy Echols <jechols@uoregon.edu>
2014-01-24 15:34:37 -0800

    Update schema.rb
    Sadly, everything is modified here because I guess we haven't run
    migrations in a while, and the new hash syntax has overwritten the
    old syntax everywhere

--- db/schema.rb
+++ db/schema.rb
@@ -9,72 +9,78 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131007151315) do
+ActiveRecord::Schema.define(version: 20140124230723) do
 
-  create_table "bookmarks", :force => true do |t|
-    t.integer  "user_id",     :null => false
+  create_table "bookmarks", force: true do |t|
+    t.integer  "user_id",     null: false
     t.string   "document_id"
     t.string   "title"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "user_type"
   end
 
-  create_table "ip_ranges", :force => true do |t|
+  create_table "ingest_file_uploads", force: true do |t|
+    t.string   "file"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ip_ranges", force: true do |t|
     t.string   "ip_start"
     t.string   "ip_end"
     t.integer  "ip_start_i"
     t.integer  "ip_end_i"
     t.integer  "role_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "ip_ranges", ["ip_end_i"], :name => "index_ip_ranges_on_ip_end_i"
-  add_index "ip_ranges", ["ip_start_i"], :name => "index_ip_ranges_on_ip_start_i"
-  add_index "ip_ranges", ["role_id"], :name => "index_ip_ranges_on_role_id"
+  add_index "ip_ranges", ["ip_end_i"], name: "index_ip_ranges_on_ip_end_i"
+  add_index "ip_ranges", ["ip_start_i"], name: "index_ip_ranges_on_ip_start_i"
+  add_index "ip_ranges", ["role_id"], name: "index_ip_ranges_on_role_id"
 
-  create_table "roles", :force => true do |t|
+  create_table "roles", force: true do |t|
     t.string "name"
   end
 
-  create_table "roles_users", :id => false, :force => true do |t|
+  create_table "roles_users", id: false, force: true do |t|
     t.integer "role_id"
     t.integer "user_id"
   end
 
-  add_index "roles_users", ["role_id", "user_id"], :name => "index_roles_users_on_role_id_and_user_id"
-  add_index "roles_users", ["user_id", "role_id"], :name => "index_roles_users_on_user_id_and_role_id"
+  add_index "roles_users", ["role_id", "user_id"], name: "index_roles_users_on_role_id_and_user_id"
+  add_index "roles_users", ["user_id", "role_id"], name: "index_roles_users_on_user_id_and_role_id"
 
-  create_table "searches", :force => true do |t|
+  create_table "searches", force: true do |t|
     t.text     "query_params"
     t.integer  "user_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "user_type"
   end
 
-  add_index "searches", ["user_id"], :name => "index_searches_on_user_id"
+  add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "guest",                  :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "guest",                  default: false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
