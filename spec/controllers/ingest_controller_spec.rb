ea9fb678b65cb8f1398a5797e2d98f14a8906fb1
Jeremy Echols <jechols@uoregon.edu>
2014-01-27 11:38:22 -0800

    Refactor ingest controller tests
    - Extracts upload behavior into shared spec for use by both the create
      and update actions
    - Reduces instance variable usage and "before" complexity by using
      `let` on more data

--- spec/controllers/ingest_controller_spec.rb
+++ spec/controllers/ingest_controller_spec.rb
@@ -5,30 +5,15 @@ require 'spec_helper'
 # either stub the map as a whole or else to traverse the real map instead of
 # hard-code attributes.
 describe IngestController do
-  before(:each) do
-    # Create a local form and stub .new so we can make assertions on what the controller sees
-    @form = Metadata::Ingest::Form.new
-    Metadata::Ingest::Form.stub(:new => @form)
+  let(:new_asset) { GenericAsset.new }
+  let(:ds_new) { new_asset.descMetadata }
+  let(:existing_asset) { GenericAsset.new }
+  let(:ds_exist) { existing_asset.descMetadata }
+  let(:form) { Metadata::Ingest::Form.new }
 
-    # Create an uploader and hack it to be the return from .new
-    @upload = IngestFileUpload.new
-    IngestFileUpload.stub(:new).and_return(@upload)
-
-    # Stub asset to avoid AF hits, create expectations, etc
-    @new_asset = GenericAsset.new
-    @ds_new = @new_asset.descMetadata
-    @new_asset.stub(:save)
-    @new_asset.stub(:save!)
-    GenericAsset.stub(:new => @new_asset)
-
-    @existing_asset = GenericAsset.new
-    @ds_exist = @existing_asset.descMetadata
-    @existing_asset.stub(:save)
-    @existing_asset.stub(:save!)
-    GenericAsset.stub(:find).with("1").and_return(@existing_asset)
-
-    # This is ugly but it mimics exactly what Rails params look like
-    @attrs = {
+  # This is ugly but it mimics exactly what Rails params look like
+  let(:attrs) do
+    {
       "titles_attributes" => {
         "0" => {"type" => "title", "value" => "test title", "internal" => "", "_destroy" => "false"}
       },
@@ -37,6 +22,17 @@ describe IngestController do
         "1" => {"type" => "subject", "value" => "bar", "internal" => "", "_destroy" => "false"}
       }
     }
+  end
+
+  before(:each) do
+    # Stub resources so we can make assertions on what the controller sees, avoid AF hits, etc
+    Metadata::Ingest::Form.stub(:new => form)
+    GenericAsset.stub(:new => new_asset)
+    new_asset.stub(:save)
+    new_asset.stub(:save!)
+    GenericAsset.stub(:find).with("1").and_return(existing_asset)
+    existing_asset.stub(:save)
+    existing_asset.stub(:save!)
   end
 
   describe "#index" do
@@ -53,44 +49,44 @@ describe IngestController do
     end
 
     it "should build an empty association for each group" do
-      for group in @form.groups
-        expect(@form).to receive(:"build_#{group}").once
+      for group in form.groups
+        expect(form).to receive(:"build_#{group}").once
       end
       get :new
     end
 
     it "shouldn't build empty associations for groups which have data" do
       # Add a single empty object to each group
-      for group in @form.groups
-        @form._build_group(group, {})
+      for group in form.groups
+        form._build_group(group, {})
       end
 
       # Neither builder should be called
-      for group in @form.groups
-        expect(@form).not_to receive(:"build_#{group}")
+      for group in form.groups
+        expect(form).not_to receive(:"build_#{group}")
       end
       get :new
     end
 
     it "should assign an uploader" do
       get :new
-      assigns(:upload).should eq(@upload)
+      assigns(:upload).should be_kind_of(IngestFileUpload)
     end
   end
 
   describe "#edit" do
     it "should have a form representing the asset" do
-      @existing_asset.title = "Title"
-      @existing_asset.descMetadata.subject = [
+      existing_asset.title = "Title"
+      existing_asset.descMetadata.subject = [
         "http://id.loc.gov/authorities/subjects/sh85050282",
         "http://id.loc.gov/authorities/subjects/sh96005121"
       ]
 
       get :edit, id: 1
 
-      sub1 = @form.subjects.first
-      sub2 = @form.subjects.last
-      title = @form.titles.first
+      sub1 = form.subjects.first
+      sub2 = form.subjects.last
+      title = form.titles.first
 
       expect(sub1.group).to eq("subject")
       expect(sub2.group).to eq("subject")
@@ -104,109 +100,68 @@ describe IngestController do
       expect(sub2.value).to eq("http://id.loc.gov/authorities/subjects/sh96005121")
       expect(title.value).to eq("Title")
 
-      expect(@form.titles.length).to eq(1)
-      expect(@form.subjects.length).to eq(2)
+      expect(form.titles.length).to eq(1)
+      expect(form.subjects.length).to eq(2)
     end
 
     it "should assign an uploader" do
       get :edit, id: 1
-      assigns(:upload).should eq(@upload)
+      assigns(:upload).should be_kind_of(IngestFileUpload)
     end
   end
 
   describe "#create" do
+    it_should_behave_like "an ingest controller uploader", :create
+
     it "should set single-value attributes to a scalar value" do
-      expect(@ds_new).to receive(:title=).with("test title")
-      post :create, :metadata_ingest_form => @attrs
+      expect(ds_new).to receive(:title=).with("test title")
+      post :create, :metadata_ingest_form => attrs
     end
 
     it "should set multiple-value attributes to arrays" do
-      expect(@ds_new).to receive(:subject=).with(["foo", "bar"])
-      post :create, :metadata_ingest_form => @attrs
+      expect(ds_new).to receive(:subject=).with(["foo", "bar"])
+      post :create, :metadata_ingest_form => attrs
     end
 
     it "shouldn't set anything on empty attributes" do
-      @attrs["titles_attributes"]["0"]["type"] = ""
-      @attrs["titles_attributes"]["0"]["value"] = ""
-      expect(@ds_new).not_to receive(:title=)
-      post :create, :metadata_ingest_form => @attrs
+      attrs["titles_attributes"]["0"]["type"] = ""
+      attrs["titles_attributes"]["0"]["value"] = ""
+      expect(ds_new).not_to receive(:title=)
+      post :create, :metadata_ingest_form => attrs
     end
 
     it "should use the internal value if one is sent" do
-      @attrs["titles_attributes"]["0"]["internal"] = "internal"
-      expect(@ds_new).to receive(:title=).with("internal")
-      post :create, :metadata_ingest_form => @attrs
+      attrs["titles_attributes"]["0"]["internal"] = "internal"
+      expect(ds_new).to receive(:title=).with("internal")
+      post :create, :metadata_ingest_form => attrs
     end
 
     it "should create a new asset" do
-      expect(@new_asset).to receive(:save).once
-      post :create, :metadata_ingest_form => @attrs
+      expect(new_asset).to receive(:save).once
+      post :create, :metadata_ingest_form => attrs
     end
 
     it "shouldn't try to modify the asset if the form isn't valid" do
-      @form.stub(:valid? => false)
-      expect(@new_asset).not_to receive(:save)
-      expect(@new_asset).not_to receive(:save!)
-      expect(@ds_new).not_to receive(:subject=)
-      expect(@ds_new).not_to receive(:title=)
-      post :update, id: 1, metadata_ingest_form: @attrs
-    end
-
-    context "when a file upload is present" do
-      let(:file) do
-        OpenStruct.new(
-          read: "contents",
-          filename: "filename",
-          file: OpenStruct.new(:content_type => "image/tiff")
-        )
-      end
-
-      before(:each) do
-        @params = {metadata_ingest_form: @attrs, id: 1, upload: "foo", upload_cache: "bar"}
-        @upload.stub(:file=)
-        @upload.stub(:file_cache=)
-        @upload.stub(:file => file)
-      end
-
-      it "should set up the @upload object" do
-        expect(@upload).to receive(:file=).with(@params[:upload])
-        expect(@upload).to receive(:file_cache=).with(@params[:upload_cache])
-        post :update, @params
-      end
-
-      it "should save the file to the asset's content datastream" do
-        expect(file).to receive(:process!)
-        expect(@existing_asset.content).to receive(:content=).with("contents")
-        expect(@existing_asset.content).to receive(:dsLabel=).with("filename")
-        expect(@existing_asset.content).to receive(:mimeType=).with("image/tiff")
-        @existing_asset.should_receive(:save)
-
-        post :update, @params
-      end
-
-      it "shouldn't alter the existing file if a new file wasn't uploaded" do
-        expect(file).not_to receive(:process!)
-        expect(@existing_asset.content).not_to receive(:content=)
-        expect(@existing_asset.content).not_to receive(:dsLabel=)
-        expect(@existing_asset.content).not_to receive(:mimeType=)
-        @existing_asset.should_receive(:save)
-
-        @params.delete(:upload)
-        @params.delete(:upload_cache)
-        post :update, @params
-      end
+      form.stub(:valid? => false)
+      expect(new_asset).not_to receive(:save)
+      expect(new_asset).not_to receive(:save!)
+      expect(ds_new).not_to receive(:subject=)
+      expect(ds_new).not_to receive(:title=)
+      post :update, id: 1, metadata_ingest_form: attrs
     end
   end
 
   describe "#update" do
+    it_should_behave_like "an ingest controller uploader", :update
+
     it "should modify the existing asset" do
-      expect(@ds_exist).to receive(:subject=).with(["foo", "bar"])
-      post :update, id: 1, metadata_ingest_form: @attrs
+      expect(ds_exist).to receive(:subject=).with(["foo", "bar"])
+      post :update, id: 1, metadata_ingest_form: attrs
     end
 
     it "should save the existing asset" do
-      expect(@existing_asset).to receive(:save).once
-      post :update, id: 1, metadata_ingest_form: @attrs
+      expect(existing_asset).to receive(:save).once
+      post :update, id: 1, metadata_ingest_form: attrs
     end
 
     it "shouldn't modify unmapped data" do
@@ -224,39 +179,39 @@ describe IngestController do
       asset.save
 
       pid = asset.pid
-      post :update, id: pid, metadata_ingest_form: @attrs
+      post :update, id: pid, metadata_ingest_form: attrs
       asset = GenericAsset.find(pid)
       rdf = asset.descMetadata.graph
       expect(rdf).to have_statement(unknown_statement)
     end
 
     it "should override data" do
-      @existing_asset.descMetadata.subject = ["foo", "bar"]
-      @attrs["subjects_attributes"] = {
+      existing_asset.descMetadata.subject = ["foo", "bar"]
+      attrs["subjects_attributes"] = {
         "0" => {"type" => "subject", "value" => "FOO", "internal" => "", "_destroy" => "false"}
       }
-      expect(@ds_exist).to receive(:subject=).with("FOO")
-      post :update, id: 1, metadata_ingest_form: @attrs
+      expect(ds_exist).to receive(:subject=).with("FOO")
+      post :update, id: 1, metadata_ingest_form: attrs
     end
 
     it "should remove data properly when blank items are submitted" do
       # This is a tricky case - by default, if there's no data for a group,
       # it won't be sent to the object, so removals are only handled well
       # when a single removal happens....
-      @existing_asset.descMetadata.subject = ["foo", "bar"]
-      @attrs["subjects_attributes"].each {|index, data| data["_destroy"] = "1"}
+      existing_asset.descMetadata.subject = ["foo", "bar"]
+      attrs["subjects_attributes"].each {|index, data| data["_destroy"] = "1"}
 
-      expect(@ds_exist).to receive(:subject=).with(nil)
-      post :update, id: 1, metadata_ingest_form: @attrs
+      expect(ds_exist).to receive(:subject=).with(nil)
+      post :update, id: 1, metadata_ingest_form: attrs
     end
 
     it "shouldn't try to modify the asset if the form isn't valid" do
-      @form.stub(:valid? => false)
-      expect(@existing_asset).not_to receive(:save)
-      expect(@existing_asset).not_to receive(:save!)
-      expect(@ds_exist).not_to receive(:subject=)
-      expect(@ds_exist).not_to receive(:title=)
-      post :update, id: 1, metadata_ingest_form: @attrs
+      form.stub(:valid? => false)
+      expect(existing_asset).not_to receive(:save)
+      expect(existing_asset).not_to receive(:save!)
+      expect(ds_exist).not_to receive(:subject=)
+      expect(ds_exist).not_to receive(:title=)
+      post :update, id: 1, metadata_ingest_form: attrs
     end
   end
 end
