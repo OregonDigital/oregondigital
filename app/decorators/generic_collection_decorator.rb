f9bdd39053935427bcada7c0fc2fa80aa374ee7f
Ryan Wick <ryanwick@gmail.com>
2014-03-10 15:13:37 -0700

    Added to_s before split, was getting test errors: no split() on nilClass

--- app/decorators/generic_collection_decorator.rb
+++ app/decorators/generic_collection_decorator.rb
@@ -10,6 +10,6 @@ class GenericCollectionDecorator < Draper::Decorator
   end
 
   def institution_class
-    descMetadata.institution.first.try(:rdf_label).try(:first).split('/').last
+    descMetadata.institution.first.try(:rdf_label).try(:first).to_s.split('/').last
   end
 end
