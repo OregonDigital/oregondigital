792bce66790c526b4e60654f6ba62719ba0d34d2
Trey Terrell <trey.terrell@oregonstate.edu>
2014-03-11 11:05:07 -0700

    Fixed repository cleaning code.

--- spec/support/rdf_repository_clean.rb
+++ spec/support/rdf_repository_clean.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
 
   config.before(:each) do
-    ActiveFedora::Rdf::Repositories.repositories.each do |repository|
+    ActiveFedora::Rdf::Repositories.repositories.each do |key, repository|
       repository.clear
     end
   end
