9e098ca320291152cbbc8ceb3b916d9c0f4c953c
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-03-18 20:42:40 -0700

    add ISO-639-1 languages

--- lib/oregon_digital/controlled_vocabularies/language.rb
+++ lib/oregon_digital/controlled_vocabularies/language.rb
@@ -2,7 +2,7 @@ module OregonDigital::ControlledVocabularies
   class Language < ActiveFedora::Rdf::Resource
     include OregonDigital::RDF::Controlled
 
+    use_vocabulary :iso_639_1
     use_vocabulary :iso_639_2
-
   end
 end
