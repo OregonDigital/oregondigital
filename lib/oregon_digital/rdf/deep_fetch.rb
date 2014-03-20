cfdf4ba261267070aff589f139778a73d0e5b78b
Trey Terrell <trey.terrell@oregonstate.edu>
2014-03-20 09:51:40 -0700

    Tightened up conditions for deep fetching.

--- lib/oregon_digital/rdf/deep_fetch.rb
+++ lib/oregon_digital/rdf/deep_fetch.rb
@@ -18,7 +18,10 @@ module OregonDigital::RDF
       if resource.rdf_label.first.blank?
         assets = ActiveFedora::Base.where("#{Solrizer.solr_name(apply_prefix(property), :facetable)}:#{RSolr.escape(resource.rdf_subject.to_s)} AND #{Solrizer.solr_name(apply_prefix("#{property}_label"), :facetable)}:[\"\" TO *]")
       else
-        assets = ActiveFedora::Base.where(Solrizer.solr_name(apply_prefix(property), :facetable) => resource.rdf_subject.to_s, "-#{Solrizer.solr_name(apply_prefix("#{property}_label"), :facetable)}" => resource.rdf_label.first.to_s).to_a
+        assets = ActiveFedora::Base.where(
+            Solrizer.solr_name(apply_prefix(property), :facetable) => resource.rdf_subject.to_s,
+            "-#{Solrizer.solr_name(apply_prefix("#{property}_label"), :facetable)}" => "#{resource.rdf_label.first}$#{resource.rdf_subject.to_s}"
+        ).where("#{Solrizer.solr_name(apply_prefix("#{property}_label"), :facetable)}:*$#{RSolr.escape(resource.rdf_subject.to_s)}")
       end
       assets.each do |a|
         a.skip_queue = 1 if a.respond_to?(:skip_queue=)
