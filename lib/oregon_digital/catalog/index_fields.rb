4f6e658c2b94c5c837278509ddce494bcdd04d02
Jeremy Echols <jechols@uoregon.edu>
2014-03-25 10:35:04 -0700

    Remove modified date from search fields

--- lib/oregon_digital/catalog/index_fields.rb
+++ lib/oregon_digital/catalog/index_fields.rb
@@ -6,7 +6,7 @@ module OregonDigital
         configure_blacklight do |config|
           # solr fields to be displayed in the index (search results) view
           #   The ordering of the field names is the order of the display
-          for field in [:description, :modified]
+          for field in [:description]
             label = OregonDigital::Metadata::FieldTypeLabel.for(field)
             config.add_index_field solr_name("desc_metadata__#{field}", :displayable), :label => label
           end
