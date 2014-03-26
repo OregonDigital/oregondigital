be501319f47fa8f551144c1199715f68da068e53
Jeremy Echols <jechols@uoregon.edu>
2014-03-26 13:30:13 -0700

    Strip out form errors not relevant to templates

--- lib/oregon_digital/metadata/template_form_container.rb
+++ lib/oregon_digital/metadata/template_form_container.rb
@@ -13,6 +13,35 @@ class OregonDigital::Metadata::TemplateFormContainer < OregonDigital::Metadata::
     prepare_data(params)
   end
 
+  # Overrides the default form validity check to only flag associations that
+  # have no type - having no value is okay on templates
+  #
+  # FIXME: This is a very hacky solution - long-term I'd like to get a form
+  # that has various validations compartmentalized.  For now, however, that
+  # would require significant changes to the form metadata gem.
+  def form_valid?
+    # If the form is completely valid, all's well
+    return true if @form.valid?
+
+    # No dice?  Go through errors looking specifically for the errors that we
+    # don't consider problematic
+    for field, error in @form.errors.dup
+      if field.to_s =~ /\.value/ && error =~ /blank/i
+        @form.errors.delete(field)
+      end
+    end
+
+    for association in @form.associations
+      for field, error in association.errors.dup
+        if field == :value && error =~ /blank/i
+          association.errors.delete(:value)
+        end
+      end
+    end
+
+    return @form.errors.empty?
+  end
+
   # Stores the template asset in AF
   def save
     @asset.save
