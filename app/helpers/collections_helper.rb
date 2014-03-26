9b80c7652f6baf27e3cd86b8201db0415bffc9b8
Trey Terrell <trey.terrell@oregonstate.edu>
2014-03-26 16:15:04 -0400

    Repair solr query.

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -5,7 +5,7 @@ module CollectionsHelper
   # TODO: We need to find a better way to do this - this means a solr call per facet. Fortunately, solr's fast.
   def controlled_view(pid)
     # Find documents which have info about this pid.
-    documents = (Blacklight.solr.get "select", :qt => "search", :q => "desc_metadata__*:#{pid}")["response"]["docs"]
+    documents = (Blacklight.solr.get "select", :params => {:qt => "search", :q => "#{RSolr.escape(pid)} -id:#{RSolr.escape(pid)}"})["response"]["docs"]
     # Get the relevant fields only.
     documents.map!{|x| x.select{|key, value| value.to_s.include?(pid)}}.reject!{|x| x.blank?}
     return "" if documents.blank?
