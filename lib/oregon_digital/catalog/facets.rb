3aed647a40d1d3b39387ec82c4dd1c582d4de4a7
Trey Terrell <trey.terrell@oregonstate.edu>
2014-03-31 13:21:00 -0700

    Changed RDF Facets to use label instead. Increases speed and reliability, but lengthens query URL.

--- lib/oregon_digital/catalog/facets.rb
+++ lib/oregon_digital/catalog/facets.rb
@@ -24,8 +24,7 @@ module OregonDigital
           # :show may be set to false if you don't want the facet to be drawn in the
           # facet bar
           controlled_vocabularies.each do |key|
-            config.add_facet_field solr_name("desc_metadata__#{key}", :facetable), :helper_method => :controlled_view, :label => I18n.t("oregondigital.catalog.facet.#{key}",:default => key.humanize)
-            config.add_facet_field solr_name("desc_metadata__#{key}_label", :facetable), :show => false
+            config.add_facet_field solr_name("desc_metadata__#{key}_label", :facetable), :helper_method => :controlled_view, :label => I18n.t("oregondigital.catalog.facet.#{key}", :default => key.humanize), :limit => 10
           end
           config.add_facet_fields_to_solr_request!
         end
