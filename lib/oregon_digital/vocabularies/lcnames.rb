bc2ba333876467861c38997e24194be141bacaf5
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-04-17 12:30:52 -0700

    introducing lc name authority

---
+++ lib/oregon_digital/vocabularies/lcnames.rb
@@ -0,0 +1,6 @@
+# This file generated automatically using vocab-fetch from http://id.loc.gov/authorities/names/
+require 'rdf'
+module OregonDigital::Vocabularies
+  class LCNAMES < ::RDF::Vocabulary("http://id.loc.gov/authorities/names/")
+  end
+end
