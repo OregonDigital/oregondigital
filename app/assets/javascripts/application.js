9de013e9fa8e3bd1bc56c60b2a4704f8f4e12e66
Trey Terrell <trey.terrell@oregonstate.edu>
2014-04-24 17:49:14 -0700

    Added advanced search plugin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require 'blacklight_advanced_search'
+
+
 //= require jquery_ujs
 //= require jquery.ui.slider
 //= require jquery.ui.draggable
