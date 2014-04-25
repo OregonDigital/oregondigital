113c3f4cd6f28b3eaf98fc15b330e1b2ce1aad9c
Trey Terrell <trey.terrell@oregonstate.edu>
2014-04-24 17:49:34 -0700

    Changed how repositories are initialized.
    Without this Advanced Search auto-loads CatalogController too early and
    fails.

--- config/initializers/rdf_repositories.rb
+++ config/initializers/rdf_repositories.rb
@@ -1,5 +1,5 @@
-
-Rails.application.config.to_prepare do
+def prepare_repositories
+  ActiveFedora::Rdf::Repositories.clear_repositories!
   if Rails.env.test?
     ActiveFedora::Rdf::Repositories.add_repository :default, RDF::Repository.new
     ActiveFedora::Rdf::Repositories.add_repository :vocabs, RDF::Repository.new
@@ -7,4 +7,8 @@ Rails.application.config.to_prepare do
     ActiveFedora::Rdf::Repositories.add_repository :default, RDF::Mongo::Repository.new
     ActiveFedora::Rdf::Repositories.add_repository :vocabs, RDF::Mongo::Repository.new(:collection => 'vocabs')
   end
+end
+prepare_repositories
+Rails.application.config.to_prepare do
+  prepare_repositories
 end
