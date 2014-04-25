ded8570fe8ee6d5a076def1e42dfc34561ea4fe1
Trey Terrell <trey.terrell@oregonstate.edu>
2014-04-25 13:38:21 -0700

    Added advanced search specs and changed patch.

--- config/initializers/blacklight_advanced_search_patch.rb
+++ config/initializers/blacklight_advanced_search_patch.rb
@@ -1,36 +1,6 @@
-module AdvancedSearchPatch
-  def get_advanced_search_facets
-    search_context_params = {}
-    adv_keys = blacklight_config.search_fields.keys.map{|k| k.to_sym}
-    trimmed_params = params.reject do |k, v|
-      adv_keys.include?(k.to_sym)
-    end
-    trimmed_params.delete(:f_inclusive)
-    search_context_params = solr_search_params(trimmed_params)
-    if advanced_search_context.length > 0
-      search_context_params.delete(:q)
-      search_context_params.delete('q')
-      search_context_params.delete_if do |k, v|
-        k = k.to_s
-        (["facet.limit", "facet.sort", "f", "facets", "facet.fields", "per_page"].include?(k) ||                
-         k =~ /f\..+\.facet\.limit/ ||
-         k =~ /f\..+\.facet\.sort/
-        )
-      end
-    end
-    input = HashWithIndifferentAccess.new
-    input.merge!(search_context_params)
-    input[:per_page] = 0
-    input[:qt] = blacklight_config.advanced_search[:qt] if blacklight_config.advanced_search[:qt]
-    input.merge!(blacklight_config.advanced_search[:form_solr_parameters]) if blacklight_config.advanced_search[:form_solr_parameters]
-    input[:q] ||= '{!lucene}*:*'
-    find(nil, input.to_hash)
-  end
-end
 module BlacklightAdvancedSearch::RenderConstraintsOverride
   def render_constraints_filters(my_params = params)
     content = super(my_params)
-
     if (@advanced_query)
       @advanced_query.filters.each_pair do |field, value_list|
         label = facet_field_labels[field]
@@ -40,10 +10,6 @@ module BlacklightAdvancedSearch::RenderConstraintsOverride
                                             )
       end
     end
-
     return content
   end
-end
-Rails.application.config.after_initialize do
-  AdvancedController.send(:include, AdvancedSearchPatch)
 end
---
+++ spec/features/advanced_search_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+describe 'Advanced Search', :focus => true do
+  let(:item) do
+    i = FactoryGirl.build(:generic_asset)
+    i.title = "Test Title"
+    i.description = "Test Description"
+    i.lcsubject = RDF::URI('http://id.loc.gov/authorities/subjects/sh85048622')
+    i.lcsubject.first << [i.lcsubject.first.rdf_subject, RDF::SKOS.prefLabel, "Test Facet"]
+    i.lcsubject.first.persist!
+    i.review!
+    i
+  end
+  before do
+    item
+  end
+  context "when you go into advanced search" do
+    before do
+      visit advanced_search_path
+    end
+    it "should show all available facets" do
+      expect(page).to have_content("Test Facet")
+    end
+    it "should not show facet URIs" do
+      expect(page).not_to have_content(item.lcsubject.first.rdf_subject.to_s)
+    end
+    context "and you search for a title" do
+      let(:search_title) {"Title"}
+      before do
+        fill_in "Title:", :with => search_title
+        within("form.advanced") do
+          click_button "Search"
+        end
+        expect(page).to have_content("You searched for")
+      end
+      context "and the document matches" do
+        it "should show" do
+          expect(page).to have_selector(".document", :count => 1)
+        end
+      end
+      context "and the document doesn't match at all" do
+        let(:search_title) {"Blabla"}
+        it "should not show" do
+          expect(page).not_to have_selector(".document")
+        end
+      end
+      context "and a different field matches for the document" do
+        let(:search_title) {"Description"}
+        it "should not show" do
+          expect(page).not_to have_selector(".document")
+        end
+      end
+    end
+    context "and you choose a facet", :js => true do
+      before do
+        find(".facet_item").click
+        expect(page).to have_content("Test Facet")
+        find("label.checkbox").click
+        within("form.advanced") do
+          click_button "Search"
+        end
+      end
+      it "should show that facet on the left" do
+        expect(page).to have_content("Any of:")
+        expect(find("span.selected")).to have_content("Test Facet")
+      end
+      it "should show it as an above facet" do
+        expect(page).to have_content("You searched for")
+        within(".results") do
+          expect(page).to have_content("Test Facet")
+        end
+      end
+      it "should not show the facet URI" do
+        expect(page).to have_content("You searched for")
+        expect(page).not_to have_content(item.lcsubject.first.rdf_subject.to_s)
+      end
+    end
+  end
+end
