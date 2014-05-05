8e1515a923ca58c5f0ea3baeb14289bd9b48e832
Trey Terrell <trey.terrell@oregonstate.edu>
2014-05-05 11:36:11 -0700

    Refactored helper to use views.

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -1,26 +1,14 @@
 module ShowHelper
   def render_download_links(document)
-    return standard_download_button(document) unless can?(:create, decorated_object.object.class)
-    content_tag("div", :class => "btn-group") do
-      link_to("#", :data => {:toggle => "dropdown"}, :class => "btn dropdown-toggle") do
-        raw("Download" + content_tag("span", '',  :class => "caret"))
-      end + 
-      content_tag("ul", :class => "dropdown-menu") do
-        download_links(document)
-      end
-    end
-  end
-  def standard_download_button(document)
-    content_tag("div") do
-      link_to "Download", download_path(document["id"]), :class => "btn"
+    if can?(:create, decorated_object.object.class)
+      render :partial => "admin_download_links"
+    else
+      render :partial => "download_links" unless can?(:create, decorated_object.object.class)
     end
   end
   def download_links(document)
-    content_tag("li") do
-      link_to "Standard", download_path(document["id"])
-    end + 
-    content_tag("li") do
-      link_to "High Resolution", download_path(document["id"], :datastream_id => "content")
-    end
+    {"Standard" => download_path(document["id"]),
+     "High Resolution" => download_path(document["id"], :datastream_id => "content")
+    }
   end
 end
