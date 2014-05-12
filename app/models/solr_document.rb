cc42065c4b91bda1b51ff887ef844f970790d083
Trey Terrell <trey.terrell@oregonstate.edu>
2014-05-12 15:13:39 -0700

    Remove unecessary formats for our solr document.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -3,32 +3,5 @@ class SolrDocument
 
   include Blacklight::Solr::Document
 
-  # self.unique_key = 'id'
-  
-  # The following shows how to setup this blacklight document to display marc documents
-  extension_parameters[:marc_source_field] = :marc_display
-  extension_parameters[:marc_format_type] = :marcxml
-  use_extension( Blacklight::Solr::Document::Marc) do |document|
-    document.key?( :marc_display  )
-  end
-  
-  # Email uses the semantic field mappings below to generate the body of an email.
-  use_extension( Blacklight::Solr::Document::Email )
-  
-  # SMS uses the semantic field mappings below to generate the body of an SMS email.
-  use_extension( Blacklight::Solr::Document::Sms )
-
-  # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
-  # Semantic mappings of solr stored fields. Fields may be multi or
-  # single valued. See Blacklight::Solr::Document::ExtendableClassMethods#field_semantics
-  # and Blacklight::Solr::Document#to_semantic_values
-  # Recommendation: Use field names from Dublin Core
-  use_extension( Blacklight::Solr::Document::DublinCore)    
-  field_semantics.merge!(    
-                         :title => "title_display",
-                         :author => "author_display",
-                         :language => "language_facet",
-                         :format => "format"
-                        )
   use_extension( OregonDigital::SolrNTripleExtension)
 end
