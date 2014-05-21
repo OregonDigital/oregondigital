039f1c7f227d2bc275b907880c98600d3c0fd538
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-05-21 16:57:45 -0700

    adding support for europeana rights

---
+++ lib/oregon_digital/vocabularies/eurights.rb
@@ -0,0 +1,11 @@
+# This file generated automatically using vocab-fetch from https://raw.github.com/OregonDigital/opaque_ns/master/eurights/rightsstatements.nt
+require 'rdf'
+module OregonDigital::Vocabularies
+  class EURIGHTS < ::RDF::StrictVocabulary("http://www.europeana.eu/rights/")
+
+    # Other terms
+    property :"rr-f/"
+    property :"rr-r/"
+    property :"unknown/"
+  end
+end
