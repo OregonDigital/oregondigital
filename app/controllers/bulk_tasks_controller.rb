c041d69b4957c44c7c2355dff6f7c26365f21604
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-07-25 13:54:56 -0700

    removing duplicate access restrictions

--- app/controllers/bulk_tasks_controller.rb
+++ app/controllers/bulk_tasks_controller.rb
@@ -2,39 +2,33 @@ class BulkTasksController < ApplicationController
   before_filter :restrict_to_archivist
   
   def index
-    restrict_to_archivist
     BulkTask.refresh
     @tasks = BulkTask.all
   end
 
   def show
-    restrict_to_archivist
     @task = BulkTask.find(params[:id])
   end
 
   def ingest
-    restrict_to_archivist
     task = BulkTask.find(params[:id])
     task.enqueue
     redirect_to bulk_tasks_path, notice: "Added #{task.directory} to the ingest queue."
   end
 
   def reset_task
-    restrict_to_archivist
     task = BulkTask.find(params[:id])
     task.reset!
     redirect_to bulk_tasks_path, notice: "Reset job: #{task.directory}."
   end
 
   def review_all
-    restrict_to_archivist
     task = BulkTask.find(params[:id])
     task.queue_review
     redirect_to bulk_tasks_path, notice: "Queued batch review of #{task.asset_ids.count} items from #{task.directory}."
   end
 
   def delete_all
-    restrict_to_archivist
     task = BulkTask.find(params[:id])
     task.queue_delete
     redirect_to bulk_tasks_path, notice: "Queued batch delete of #{task.asset_ids.count} items from #{task.directory}."
