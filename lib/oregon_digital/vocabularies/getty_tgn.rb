676efab633b9277ba84ef3f1ce7d879bff7f599c
Jeremy Echols <jechols@uoregon.edu>
2014-08-29 13:08:26 -0700

    Sync to latest vocabularies

---
+++ lib/oregon_digital/vocabularies/getty_tgn.rb
@@ -0,0 +1,6 @@
+# This file generated automatically using vocab-fetch from http://vocab.getty.edu/tgn/
+require 'rdf'
+module OregonDigital::Vocabularies
+  class GETTY_TGN < ::RDF::Vocabulary("http://vocab.getty.edu/tgn/")
+  end
+end
---
+++ lib/oregon_digital/vocabularies/tgm.rb
@@ -0,0 +1,6 @@
+# This file generated automatically using vocab-fetch from http://id.loc.gov/vocabulary/graphicMaterials
+require 'rdf'
+module OregonDigital::Vocabularies
+  class TGM < ::RDF::Vocabulary("http://id.loc.gov/vocabulary/graphicMaterials")
+  end
+end
