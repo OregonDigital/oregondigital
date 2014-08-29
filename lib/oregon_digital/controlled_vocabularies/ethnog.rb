66043c241f1849b6dfa15690a5ccf6f615311e5a
Karen Estlund <kestlund@gmail.com>
2014-08-29 11:54:26 -0700

    added ethnographic class

---
+++ lib/oregon_digital/controlled_vocabularies/ethnog.rb
@@ -0,0 +1,24 @@
+module OregonDigital::ControlledVocabularies
+  class Subject < ActiveFedora::Rdf::Resource
+    include OregonDigital::RDF::Controlled
+
+    use_vocabulary :afs_ethn
+
+    class QaLcsh < Qa::Authorities::Loc
+      include OregonDigital::Qa::Caching
+      def search(q, sub_authority=nil)
+        super(q, 'ethnog')
+      end
+
+      def loc_response_to_qa(data)
+        response = super(data)
+        for link in data.links
+          response["id"] = link[1] if link[0].nil?
+        end
+        return response
+      end
+    end
+
+    @qa_interface = QaLcsh.new
+  end
+end
