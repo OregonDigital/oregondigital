562d0f2397ed965b1ccac8ee73e891139f12c35d
Jeremy Echols <jechols@uoregon.edu>
2014-09-04 10:31:05 -0700

    Add aat vocabulary to WorkType CV

--- lib/oregon_digital/controlled_vocabularies/work_type.rb
+++ lib/oregon_digital/controlled_vocabularies/work_type.rb
@@ -2,6 +2,7 @@ module OregonDigital::ControlledVocabularies
   class WorkType < ActiveFedora::Rdf::Resource
     include OregonDigital::RDF::Controlled
 
+    use_vocabulary :aat
     use_vocabulary :worktype
 
   end
