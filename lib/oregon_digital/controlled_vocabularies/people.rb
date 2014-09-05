220bfdc0de8f204be348ac9a36cb121083223ef1
Jeremy Echols <jechols@uoregon.edu>
2014-09-05 15:25:55 -0700

    Add lcnames to people CV

--- lib/oregon_digital/controlled_vocabularies/people.rb
+++ lib/oregon_digital/controlled_vocabularies/people.rb
@@ -3,6 +3,16 @@ module OregonDigital::ControlledVocabularies
     include OregonDigital::RDF::Controlled
 
     use_vocabulary :people
+    use_vocabulary :lcnames
+
+    class QaLcNames < Qa::Authorities::Loc
+      include OregonDigital::Qa::Caching
+      def search(q, sub_authority=nil)
+        super(q, 'names')
+      end
+    end
+
+    @qa_interface = QaLcNames.new
 
   end
 end
