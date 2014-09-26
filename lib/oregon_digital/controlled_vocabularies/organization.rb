cf7611a372cd250fd4591821217d42ebde7f4c44
Karen Estlund <kestlund@gmail.com>
2014-09-26 14:18:26 -0700

    removed no longer used vocab in or-univ

--- lib/oregon_digital/controlled_vocabularies/organization.rb
+++ lib/oregon_digital/controlled_vocabularies/organization.rb
@@ -1,7 +1,6 @@
 module OregonDigital::ControlledVocabularies
   class Organization < ActiveFedora::Rdf::Resource
     include OregonDigital::RDF::Controlled
-    use_vocabulary :oregon_universities
     use_vocabulary :institutions
 
     property :label, :predicate => RDF::RDFS.label
