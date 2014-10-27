9b56df3af679c8ea5bd46fabe086d08210396764
Trey Terrell <trey.terrell@oregonstate.edu>
2014-10-27 11:18:54 -0700

    Make guest users have their roles checked.

---
+++ spec/features/ip_roles_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "IP Roles" do
+  context "when an item is restricted to an IP" do
+    let(:role) {FactoryGirl.create(:role)}
+    let(:ip_range) {IpRange.create(:role => role, :ip_start => ip, :ip_end => ip)}
+    let(:ip) {"128.0.0.0"}
+    let(:asset) do
+      a = FactoryGirl.create(:generic_asset)
+      a.read_groups = [role.name]
+      a.save
+      a
+    end
+    before do
+      ip_range
+      asset
+    end
+    context "as a guest user" do
+      context "who has the right IP" do
+        before do
+          ActionDispatch::Request.any_instance.stub(:remote_ip).and_return(ip)
+          visit catalog_path(asset.pid)
+        end
+        it "should be viewable" do
+          expect(page).to have_content asset.title
+        end
+      end
+      context "who has the wrong IP" do
+        before do
+          visit catalog_path(asset.pid)
+        end
+        it "should not be viewable" do
+          expect(page).not_to have_content(asset.title)
+        end
+      end
+    end
+  end
+end
