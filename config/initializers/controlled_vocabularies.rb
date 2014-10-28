e4ef891c9cfae63115c5d97d5435ce29f007184e
Karen Estlund <kestlund@gmail.com>
2014-10-28 14:46:17 -0700

    added bibframe as a vocab

--- config/initializers/controlled_vocabularies.rb
+++ config/initializers/controlled_vocabularies.rb
@@ -41,5 +41,6 @@ RDF_VOCABS = {
   :ubio                 =>  { :prefix => 'http://identifiers.org/ubio.namebank/', :strict => false, :fetch => false },
   :itis                 =>  { :prefix => 'http://www.itis.gov/ITISWebService/', :strict => false, :fetch => false },
   :bm                   =>  { :prefix => 'http://collection.britishmuseum.org/', :strict => false, :fetch => false },
-  :archiveshub          =>  { :prefix => 'http://data.archiveshub.ac.uk/def/', :source => 'http://data.archiveshub.ac.uk/def/' }
+  :archiveshub          =>  { :prefix => 'http://data.archiveshub.ac.uk/def/', :source => 'http://data.archiveshub.ac.uk/def/' },
+  :bibframe             =>  { :prefix => 'http://bibframe.org/vocab/', :source => 'http://bibframe.org/vocab/', :strict => false, :fetch => false}
 }
