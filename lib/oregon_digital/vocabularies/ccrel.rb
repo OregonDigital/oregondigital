727f60e1b2cc8df18832ec62b7081015c7aa140a
Jeremy Echols <jechols@uoregon.edu>
2014-10-29 17:35:36 +0000

    Add new vocabs

---
+++ lib/oregon_digital/vocabularies/ccrel.rb
@@ -0,0 +1,6 @@
+# This file generated automatically using vocab-fetch from http://creativecommons.org/ns
+require 'rdf'
+module OregonDigital::Vocabularies
+  class CCREL < ::RDF::Vocabulary("http://creativecommons.org/ns")
+  end
+end
