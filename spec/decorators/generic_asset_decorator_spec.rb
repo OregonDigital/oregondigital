5a80506a0b0739eb23038cd9c58f1d75f556237d
Trey Terrell <trey.terrell@oregonstate.edu>
2014-12-05 13:43:29 -0800

    Attempt to use view specs to deal with Capybara.

--- spec/decorators/generic_asset_decorator_spec.rb
+++ spec/decorators/generic_asset_decorator_spec.rb
@@ -4,6 +4,47 @@ describe GenericAssetDecorator do
   let(:subject) {GenericAssetDecorator.new(asset)}
   let(:asset) {FactoryGirl.build(:generic_asset)}
   let(:result) {subject.sorted_show_fields}
+
+  describe "#view_partial" do
+    let(:asset) { inner.decorate }
+    context "an image with jpeg content" do
+      let(:inner) { FactoryGirl.build(:image, :with_jpeg_datastream) }
+      it "should be small image viewer" do
+        expect(asset.view_partial).to eq "small_image_viewer"
+      end
+    end
+    context "an image with a large tiff" do
+      let(:inner) { FactoryGirl.build(:image, :with_tiff_datastream) }
+      it "should be large image viewer" do
+        expect(asset.view_partial).to eq "large_image_viewer"
+      end
+    end
+    context "a document" do
+      let(:inner) { FactoryGirl.build(:document) }
+      it "should be document viewer" do
+        expect(asset.view_partial).to eq "document_viewer"
+      end
+    end
+    context "a generic asset" do
+      let(:inner) { FactoryGirl.build(:generic_asset) }
+      it "should be a generic viewer" do
+        expect(asset.view_partial).to eq "generic_viewer"
+      end
+    end
+    context "an audio asset" do
+      let(:inner) { FactoryGirl.build(:audio) }
+      it "should be an audio viewer" do
+        expect(asset.view_partial).to eq "audio_viewer"
+      end
+    end
+    context "a video asset" do
+      let(:inner) { FactoryGirl.build(:video) }
+      it "should be an audio viewer" do
+        expect(asset.view_partial).to eq "video_viewer"
+      end
+    end
+  end
+
   describe "#sorted_show_fields" do
     before(:each) do
       I18n.stub(:t).and_call_original
