e624b1f8ef63406e4c95186b337bddcca2081c9a
Trey Terrell <trey.terrell@oregonstate.edu>
2014-12-08 16:46:28 -0800

    Check groups caused by deleting, not permissions in controller.

---
+++ spec/lib/oregon_digital/soft_destroy_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "#soft_destroy" do
+  let(:asset) { FactoryGirl.build(:generic_asset) }
+  before do
+    asset.stub(:save)
+    asset.soft_destroy
+  end
+  it "should set #soft_destroyed?" do
+    expect(asset).to be_soft_destroyed
+  end
+  it "should reset workflow" do
+    expect(asset).to receive(:reset_workflow)
+    asset.soft_destroy
+  end
+
+  describe "#undelete" do
+    it "should not alter groups" do
+      before_groups = asset.read_groups
+      asset.undelete
+      expect(asset.read_groups).to eq before_groups
+    end
+    it "should no longer be destroyed" do
+      asset.undelete
+      expect(asset).not_to be_soft_destroyed
+    end
+  end
+end
