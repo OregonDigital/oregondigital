439d8c2744ff1dd1cf6f74b718ca6588bb6712ab
Jeremy Echols <jechols@uoregon.edu>
2015-02-11 08:53:57 -0800

    Don't throw Internal Server Error on access denied
    Redirects access denied errors using hydra controller behavior code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,16 @@ class ApplicationController < ActionController::Base
   layout 'blacklight'
 
   protect_from_forgery
+
+  # Copied from hydra controller behaviors
+  rescue_from Hydra::AccessDenied do |exception|
+    if (exception.action == :edit)
+      redirect_to({:action=>'show'}, :alert => exception.message)
+    elsif current_user and current_user.persisted?
+      redirect_to root_url, :alert => exception.message
+    else
+      session["user_return_to"] = request.url
+      redirect_to new_user_session_url, :alert => exception.message
+    end
+  end
 end
