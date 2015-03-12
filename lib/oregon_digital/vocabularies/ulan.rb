be017b5589909ce5656da0478840067b6c970f71
lsat12357 <lsato@uoregon.edu>
2015-03-12 18:28:15 +0000

    gen_vocabs

---
+++ lib/oregon_digital/vocabularies/ulan.rb
@@ -0,0 +1,6 @@
+# This file generated automatically using vocab-fetch from http://vocab.getty.edu/ulan/
+require 'rdf'
+module OregonDigital::Vocabularies
+  class ULAN < ::RDF::Vocabulary("http://vocab.getty.edu/ulan/")
+  end
+end
