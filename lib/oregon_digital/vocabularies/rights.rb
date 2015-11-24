71034360466e71396b27b6dd4253afd5a33eae93
Linda Sato <lsato@uoregon.edu>
2015-11-24 15:17:12 -0800

    adding ns to rights in vocabularies

--- lib/oregon_digital/vocabularies/rights.rb
+++ lib/oregon_digital/vocabularies/rights.rb
@@ -1,7 +1,7 @@
 # This file generated automatically using vocab-fetch from https://raw.github.com/OregonDigital/opaque_ns/master/rights.jsonld
 require 'rdf'
 module OregonDigital::Vocabularies
-  class RIGHTS < ::RDF::StrictVocabulary("http://opaquenamespace.org/rights/")
+  class RIGHTS < ::RDF::StrictVocabulary("http://opaquenamespace.org/ns/rights/")
 
     # Other terms
     property :educational, :label => 'Educational Use Permitted'
