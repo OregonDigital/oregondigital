e31419f808bdf56cdcd1272f74354943b1573d53
Linda Sato <lsato@uoregon.edu>
2015-12-10 14:58:04 -0800

    add sets to records

--- lib/oregon_digital/oai/model/solr_instance_decorator.rb
+++ lib/oregon_digital/oai/model/solr_instance_decorator.rb
@@ -1,7 +1,7 @@
 class OregonDigital::OAI::Model::SolrInstanceDecorator < Draper::Decorator
 
     delegate_all
-    attr_accessor :modified_date, :identifier
+    attr_accessor :modified_date, :identifier, :sets
 
     #override fields that return uris to return labels
     def set_attrs (key, val)
