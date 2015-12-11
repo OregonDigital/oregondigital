64afbd421345405c63ed6bec1398006605387696
Linda Sato <lsato@uoregon.edu>
2015-12-11 15:37:28 -0800

    reset limit

--- lib/oregon_digital/oai/provider.rb
+++ lib/oregon_digital/oai/provider.rb
@@ -4,7 +4,7 @@ class OregonDigital::OAI::Provider < ::OAI::Provider::Base
   record_prefix APP_CONFIG['oai']['record_prefix']
   admin_email APP_CONFIG['oai']['admin_email']
   sample_id APP_CONFIG['oai']['sample_id']
-  source_model ::OregonDigital::OAI::Model::ActiveFedoraWrapper.new(::ActiveFedora::Base, :limit => 5)
+  source_model ::OregonDigital::OAI::Model::ActiveFedoraWrapper.new(::ActiveFedora::Base, :limit => 10)
 
   Base.register_format(OregonDigital::OAI::DublinCore.instance)
 end
