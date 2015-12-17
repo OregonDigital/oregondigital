6eac7b07280756c2b22153c22fb5dbc690497114
Ryan Wick <ryan.wick@oregonstate.edu>
2015-12-17 15:29:41 -0800

    Added map_oai_qdc mapping. Added some to uri_fields.

--- lib/oregon_digital/oai/concern.rb
+++ lib/oregon_digital/oai/concern.rb
@@ -5,15 +5,40 @@ module OregonDigital::OAI::Concern
       Time.parse(modified_date).utc
     end
   end
+
   module ClassMethods
     def string_fields
-      @string_fields = [ :title, :description, :date, :identifier]
+      @string_fields = [:title, :description, :date, :identifier]
     end
     def uri_fields
-      @uri_fields = [:creator, :lcsubject, :type, :format, :rights]
+      @uri_fields = [:creator, :lcsubject, :type, :format, :rights, :location, :author, :editor, :photographer, :rangerDistrict, :set]
     end
+
     def mapped_fields
-      @mapped_fields = {:lcsubject => "subject"}
+      @mapped_fields = {#:lcsubject => "subject",
+#                        :location => "coverage",
+#                        :photographer => "creator2"
+      }
+    end
+
+    # Map Oregon Digital fields to Qualified Dublin Core fields
+    def map_oai_qdc
+      { :title => [:title],
+        :description => [:description],
+        :identifier => [:identifier],
+        :date => [:date],
+        :created => [:created],
+        :issued => [:issued],
+        :creator => [:creator],
+        :contributor => [:arranger, :artist, :author, :cartographer, :collector, :composer, :contributor, :donor, :editor, :photographer],
+        :subject => [:lcsubject, :subject],
+        :rights => [:rights],
+        :spatial => [:location, :tgn, :waterBasin, :rangerDistrict, :streetAddress],
+        :type => [:type],
+#        :format => [:format],
+        :language => [:language],
+        :isPartOf => [:set]
+      }
     end
   end
 end
