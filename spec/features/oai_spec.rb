145fdc526afbf2c678644a07551894ff90dd3b99
Ryan Wick <ryan.wick@oregonstate.edu>
2015-12-23 12:46:58 -0800

    Changed rspec for OAI to use shared examples and run on both oai_dc and oai_qdc response formats.

--- spec/features/oai_spec.rb
+++ spec/features/oai_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
-describe "OAI endpoint" do
+shared_examples "OAI endpoint" do |parameter|
   let(:asset_class) {GenericAsset}
+
   context "when there are assets" do
     let(:lcname) {RDF::URI.new("http://id.loc.gov/authorities/names/nr93013379")}
     let(:lcname2) {RDF::URI.new("http://id.loc.gov/authorities/names/no00013511")}
     let(:lcsubj) {RDF::URI.new("http://id.loc.gov/authorities/subjects/sh2003003075")}
     let(:asset) {asset_class.new}
+
     before(:each) do
       asset.title = "Test Title"
       asset.descMetadata.creator = [lcname,lcname2]
@@ -21,9 +23,10 @@ describe "OAI endpoint" do
       asset.save
       asset.review!
     end
+
     context "and they are generic assets" do
       before :each do
-        visit oai_path(:verb => "ListRecords", :metadataPrefix => "oai_dc")
+        visit oai_path(:verb => "ListRecords", :metadataPrefix => parameter)
       end
       it "should show them" do
         expect(page).to have_content("Test Title")
@@ -48,33 +51,44 @@ describe "OAI endpoint" do
         expect(page).not_to have_xpath('//lcsubject')
       end
     end
+
     context "if one of them is deleted" do
       before do
         asset.soft_destroy
         asset.save
-        visit oai_path(:verb => "ListRecords", :metadataPrefix => "oai_dc")
+        visit oai_path(:verb => "ListRecords", :metadataPrefix => parameter)
       end
       it "should have 'deleted' in the header of the record" do
         expect(page).to have_xpath('//header[@status="deleted"]')
       end
     end
+
     context "and they are documents" do
       let(:asset_class) {Document}
       before do
-        visit oai_path(:verb => "ListRecords", :metadataPrefix => "oai_dc")
+        visit oai_path(:verb => "ListRecords", :metadataPrefix => parameter)
       end
       it "should show them" do
         expect(page).to have_content("Test Title")
       end
     end
+
     context "and they are not reviewed" do
       before do
         asset.reset_workflow!
-        visit oai_path(:verb => "ListRecords", :metadataPrefix => "oai_dc")
+        visit oai_path(:verb => "ListRecords", :metadataPrefix => parameter)
       end
       it "should not show them" do
         expect(page).not_to have_content("Test Title")
       end
     end
   end
+end
+
+describe "Dublin Core response" do
+  it_behaves_like "OAI endpoint", "oai_dc"
+end
+
+describe "Qualified Dublin Core response" do
+  it_behaves_like "OAI endpoint", "oai_qdc"
 end
