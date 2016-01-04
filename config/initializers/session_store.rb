7b808804f60e5f6d6801ae2602c7e3e908e1c2fd
Jeremy Echols <jechols@uoregon.edu>
2016-01-04 08:53:59 -0800

    Use cache store for sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Oregondigital::Application.config.session_store :cookie_store, key: '_oregondigital_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Oregondigital::Application.config.session_store :active_record_store
+Oregondigital::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.days
