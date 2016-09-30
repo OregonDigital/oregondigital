a5749bd86cbb54314b2d7713318a4544bf054294
lsato <lsato@lib-adk0004.uoregon.edu>
2016-09-30 15:58:42 -0700

    downloads hack

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -2,6 +2,8 @@ module ShowHelper
   def render_download_links(document)
     if can?(:create, decorated_object.object.class)
       render :partial => "admin_download_links"
+    elsif ((can?(:read, decorated_object.object.class))&&(decorated_object.object.read_groups.include? "University-of-Oregon"))
+      render :partial => "admin_download_links"
     else
       render :partial => "download_links" unless can?(:create, decorated_object.object.class)
     end
