37107cff09c3312e658369a90f4d85fda52e4fdd
Jeremy Echols <jechols@uoregon.edu>
2016-10-05 15:10:35 -0700

    Only fry web container when running dev.sh

--- docker/dev.sh
+++ docker/dev.sh
@@ -3,8 +3,8 @@
 # This script is primarily just for demonstrating what needs to happen to get
 # set up.  Some things may need to be tweaked, such as only migrating once.
 # Feel free to copy this to create your own docker-compose adventure!
-docker-compose -f docker/docker-compose.dev.yaml stop
-docker-compose -f docker/docker-compose.dev.yaml rm -f
+docker-compose -f docker/docker-compose.dev.yaml stop web
+docker-compose -f docker/docker-compose.dev.yaml rm -f web
 
 docker build --rm -t oregondigital/od1 -f docker/Dockerfile-dev .
 docker-compose -f docker/docker-compose.dev.yaml run web bundle exec rake db:migrate
