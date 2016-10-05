d07d1e869a37d5969f5986d0c9346d125a1c75ab
Jeremy Echols <jechols@uoregon.edu>
2016-10-05 11:31:02 -0700

    Add sample script to start/restart services

---
+++ docker/dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# This script is primarily just for demonstrating what needs to happen to get
+# set up.  Some things may need to be tweaked, such as only migrating once.
+# Feel free to copy this to create your own docker-compose adventure!
+docker-compose -f docker/docker-compose.dev.yaml stop
+docker-compose -f docker/docker-compose.dev.yaml rm -f
+
+docker build --rm -t oregondigital/od1 -f docker/Dockerfile-dev .
+docker-compose -f docker/docker-compose.dev.yaml run web bundle exec rake db:migrate
+docker-compose -f docker/docker-compose.dev.yaml run web rm -rf /oregondigital/tmp/pids
+docker-compose -f docker/docker-compose.dev.yaml up
+
+# Need a shell in the web head?  Try this:
+# docker-compose -f docker/docker-compose.dev.yaml run web bash
