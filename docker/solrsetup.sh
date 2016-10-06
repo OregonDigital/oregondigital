dd0e1603cc529aea10e5c5aec8eb039b24de13f1
Jeremy Echols <jechols@uoregon.edu>
2016-10-06 11:03:24 -0700

    Replace Jetty Solr 4.0 with official Solr 6 image

---
+++ docker/solrsetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Modified from solr-precreate task in distribution's script
+CORE=development
+CONFIG_SOURCE="/opt/solr/server/solr/configsets/data_driven_schema_configs"
+coresdir="/opt/solr/server/solr/mycores"
+mkdir -p $coresdir
+coredir="$coresdir/$CORE"
+if [[ ! -d $coredir ]]; then
+    cp -r $CONFIG_SOURCE/ $coredir
+    touch "$coredir/core.properties"
+    echo created "$CORE"
+else
+    echo "core $CORE already exists"
+fi
+
+# Override stuff with OD files
+rm -f $coredir/conf/managed-schema
+cp /var/odconf/conf/schema.xml $coredir/conf/schema.xml
+cp /var/odconf/conf/solrconfig.xml $coredir/conf/solrconfig.xml
