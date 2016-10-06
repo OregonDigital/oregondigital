a359b35d1c6279f0b38dad5bbca4b4b488d85741
Jeremy Echols <jechols@uoregon.edu>
2016-10-06 15:08:14 -0700

    Fix dev.sh yet again

--- docker/dev.sh
+++ docker/dev.sh
@@ -3,12 +3,14 @@
 # This script is primarily just for demonstrating what needs to happen to get
 # set up.  Some things may need to be tweaked, such as only migrating once.
 # Feel free to copy this to create your own docker-compose adventure!
-./docker/compose stop web
-./docker/compose rm -f web
 
+# Stop everything
+./docker/compose stop
+
+# Build, migrate, and clean up
 docker build --rm -t oregondigital/od1 -f docker/Dockerfile-dev .
 ./docker/compose run web bundle exec rake db:migrate
-./docker/compose start
+./docker/compose rm -f web
 
-# Need a shell in the web head?  Try this:
-# docker-compose -f docker/docker-compose.dev.yaml run web bash
+# Restart
+./docker/compose up
