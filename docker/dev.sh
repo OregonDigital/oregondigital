781aec4a71df7f5c8b34119242d2620b0ca6c671
Jeremy Echols <jechols@uoregon.edu>
2016-10-06 08:43:07 -0700

    Make a shortcut for all docker-compose commands
    Sets up a better project name and uses the dev compose file

--- docker/dev.sh
+++ docker/dev.sh
@@ -3,12 +3,12 @@
 # This script is primarily just for demonstrating what needs to happen to get
 # set up.  Some things may need to be tweaked, such as only migrating once.
 # Feel free to copy this to create your own docker-compose adventure!
-docker-compose -f docker/docker-compose.dev.yaml stop web
-docker-compose -f docker/docker-compose.dev.yaml rm -f web
+./docker/compose stop web
+./docker/compose rm -f web
 
 docker build --rm -t oregondigital/od1 -f docker/Dockerfile-dev .
-docker-compose -f docker/docker-compose.dev.yaml run web bundle exec rake db:migrate
-docker-compose -f docker/docker-compose.dev.yaml up
+./docker/compose run web bundle exec rake db:migrate
+./docker/compose start
 
 # Need a shell in the web head?  Try this:
 # docker-compose -f docker/docker-compose.dev.yaml run web bash
