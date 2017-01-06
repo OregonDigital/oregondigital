486c0406df3eddda22fb92f73be7a73819aff490
Jeremy Echols <jechols@uoregon.edu>
2017-01-06 08:43:08 -0800

    Remove docker/compose "helper"
    It's not very helpful, actually

--- docker/dev.sh
+++ docker/dev.sh
@@ -5,14 +5,14 @@
 # Feel free to copy this to create your own docker-compose adventure!
 
 # Stop everything
-./docker/compose stop
+docker-compose stop
 
 # Build, migrate, and clean up
 docker build --rm -t oregondigital/od1 -f docker/Dockerfile-dev .
-./docker/compose run web bundle exec rake db:migrate
-./docker/compose rm -f web
-./docker/compose rm -f workers
-./docker/compose rm -f resquehead
+docker-compose run web bundle exec rake db:migrate
+docker-compose rm -f web
+docker-compose rm -f workers
+docker-compose rm -f resquehead
 
 # Restart
-./docker/compose up
+docker-compose up
