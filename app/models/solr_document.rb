cb3f5fd7df51380fb8bff5da93f3ff4a93f275ff
Jeremy Echols <jechols@uoregon.edu>
2017-01-06 14:57:57 -0800

    Tell Rails how to turn a SolrDocument into a model
    It magicked this previously, but something changed in polymorphic_url
    and/or its callees.  This fixes many test failures.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,7 +1,10 @@
 # -*- encoding : utf-8 -*-
 class SolrDocument 
-
   include Blacklight::Solr::Document
 
   use_extension( OregonDigital::SolrNTripleExtension)
+
+  def to_model
+    return ActiveFedora::Base.load_instance_from_solr(self["id"], self)
+  end
 end
