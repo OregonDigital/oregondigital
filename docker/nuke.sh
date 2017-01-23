7b36507d831116da8699a950b786078b8ae57b01
Jeremy Echols <jechols@uoregon.edu>
2017-01-23 14:58:56 -0800

    Make awesome colors for docker/nuke.sh script

--- docker/nuke.sh
+++ docker/nuke.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 #
 # Forcibly destroys all OD docker containers and volumes
+csi=$(echo -e "\x1b[")
+warn="${csi}31;1m"
+info="${csi}34m"
+reset="${csi}0m"
+echo -ne $warn
+echo "Stopping and removing all containers related to OregonDigital development"
+echo -ne $reset$info
+echo "(Ignore any errors that occur, as docker is very verbose when you try to"
+echo "stop or remove anything already stopped/removed)"
+echo -ne $reset
 docker stop $(docker ps -qaf "name=oregondigital*")
 docker stop $(docker ps -qaf "name=odtest*")
 docker rm -f $(docker ps -qaf "name=oregondigital*")
