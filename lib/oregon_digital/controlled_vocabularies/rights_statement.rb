7103d00fdd9a6f18627138ea125aa81a0baf966e
Ryan Wick <ryan.wick@oregonstate.edu>
2017-02-06 18:04:33 -0800

    Remove Europeana rights config from system
    Remove the Europeana rights vocab, remove it from the Rights controlled
    vocabulary and update fixtures.

--- lib/oregon_digital/controlled_vocabularies/rights_statement.rb
+++ lib/oregon_digital/controlled_vocabularies/rights_statement.rb
@@ -5,7 +5,6 @@ module OregonDigital::ControlledVocabularies
     configure :rdf_label => RDF::DC11.title
 
     use_vocabulary :rights
-    use_vocabulary :eurights
     use_vocabulary :cclicenses
     use_vocabulary :ccpublic
 
