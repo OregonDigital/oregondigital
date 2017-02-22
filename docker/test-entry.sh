65c1616de6b77ed62cb9c0949428dac947257b24
Jeremy Echols <jechols@uoregon.edu>
2017-02-22 14:43:14 -0800

    Don't delete set_content when testing

--- docker/test-entry.sh
+++ docker/test-entry.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# Re-link the set content, since dev will mount in local directories
-/bin/rm -f /oregondigital/set_content
-/bin/ln -s /opt/od_set_content /oregondigital/set_content
-/link-set-content.sh
-
 # run tests
 bundle exec rspec $@
