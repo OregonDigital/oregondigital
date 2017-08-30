c674f7a3b23731a3f9cf032d6b8b68a76b0f3a80
Ryan Wick <ryan.wick@oregonstate.edu>
2017-08-30 16:11:18 -0700

    Add custom fetch method for dbpedia.org URIs
    This forces the downloading of the RDF/XML which doesn't have the
    rdf:langString error issues that we were seeing with JSON and n-triples.

--- lib/oregon_digital/controlled_vocabularies/organization.rb
+++ lib/oregon_digital/controlled_vocabularies/organization.rb
@@ -1,3 +1,5 @@
+require 'rest_client'
+
 module OregonDigital::ControlledVocabularies
   class Organization < ActiveFedora::Rdf::Resource
     include OregonDigital::RDF::Controlled
@@ -6,5 +8,28 @@ module OregonDigital::ControlledVocabularies
     use_vocabulary :creator
 
     property :label, :predicate => RDF::RDFS.label
+
+    # Custom fetch methods for dbpedia.org URIs
+    # Fetch began failing, due to error with rdf:LangString not having a language, in JSON and n-triples
+    # This forces the download of the RDF/XML which doesn't have this issue
+    def fetch
+      if self.rdf_subject.to_s.include?('dbpedia') then
+        original_uri = self.rdf_subject.to_s
+        rdfxml_uri = self.rdf_subject.to_s
+        rdfxml_uri = rdfxml_uri.gsub('resource', 'data').concat('.rdf')
+
+        label_xpath = "/rdf:RDF/rdf:Description[@rdf:about='" + original_uri + "']/rdfs:label[@xml:lang='en']/text()"
+
+        response = RestClient.get rdfxml_uri
+        xmldoc = Nokogiri::XML(response)
+        new_label = xmldoc.at_xpath(label_xpath).to_s
+
+        self << RDF::Statement.new(rdf_subject, RDF::SKOS.prefLabel, new_label) unless new_label.empty?
+
+        persist!
+      else
+        super
+      end
+    end
   end
 end
