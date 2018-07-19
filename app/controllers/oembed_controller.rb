73137cda1e7f3b2017620290808a5b5bef6c6904
lsat12357 <lsato@uoregon.edu>
2018-07-19 09:31:53 -0700

    use solr

--- app/controllers/oembed_controller.rb
+++ app/controllers/oembed_controller.rb
@@ -6,12 +6,10 @@ class OembedController < ApplicationController
     pid = find_pid.to_s
 
     begin
-      asset = GenericAsset.find(pid)
-
-      return render_401 unless is_public(asset)
+      return render_401 unless is_public
       
-      if is_image(asset)
-        return image_responder(pid, params[:format])
+      if is_image
+        return image_responder(params[:format])
       else
         return other_responder
       end
@@ -21,31 +19,26 @@ class OembedController < ApplicationController
     end
   end
 
-  def image_responder(pid, format)
-
+  def image_responder(format)
+    location = "/media/medium-images/#{loc}/#{modified_pid}.jpg"
     begin
-      asset = Image.find(pid)
-      return render_404 unless ((!asset.medium_image_location.blank?) && (File.exist? asset.medium_image_location))
-
-      img = MiniMagick::Image.open(asset.medium_image_location)
+      img = MiniMagick::Image.open(location)
       data = {
         "version" => "1.0",
         "type" => "photo",
         "width" => img['width'],
         "height" => img['height'], 
-        "url" => "#{APP_CONFIG['default_url_host']}#{asset.medium_image_location.sub(Rails.root.to_s, '')}"
+        "url" => "#{APP_CONFIG['default_url_host']}#{location}"
       }
-
       if format == 'json'
         json_response(data)
       else
         other_response
       end
     rescue
-      return render_404
+      raise
     end
   end
-
 
   def other_responder
     render_501
@@ -60,8 +53,8 @@ class OembedController < ApplicationController
     render body: JSON.generate(data)
   end
 
-  def is_image (asset)
-    asset.to_solr["has_model_ssim"].include? "Image"
+  def is_image
+    solr_doc["has_model_ssim"].include? "Image"
   end
 
   def render_404
@@ -76,8 +69,46 @@ class OembedController < ApplicationController
     render :text => "Not Implemented", :status => 501
   end
 
-  def is_public(asset)
-    asset.read_groups.include? "public"
+ def is_public
+    solr_doc["read_access_group_ssim"].include? "public"
+    rescue
+      raise
+  end
+
+  #fwc will return an empty array if nothing found
+  def solr_doc
+    @solr_doc ||= ActiveFedora::Base.find_with_conditions({:id=>pid}).first
+    if @solr_doc.blank?
+      raise OembedSolrError
+    else
+      @solr_doc
+    end
+  end
+
+  def json
+    begin
+      JSON.parse(solr_doc["object_profile_ssm"].first)
+    rescue JSON::ParserError
+      raise
+    end
+  end
+
+  def path
+    json["datastreams"]["thumbnail"]["dsLocation"]
+    rescue
+      raise
+  end
+
+  #match will return nil, but to_s on nil returns empty array
+  def loc
+    result = /thumbnails\/[a-z0-9]\/[a-z0-9]/.match path
+    if result.nil?
+      raise OembedSolrError
+    end
+    result.to_s.gsub("thumbnails/","")
+  end
+
+  class OembedSolrError < StandardError
   end
 
 end
