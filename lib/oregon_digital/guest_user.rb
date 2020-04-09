b9dbf32e2fa899f7524e02f543097d2bbed4f626
Jeremy Echols <jechols@uoregon.edu>
2020-04-09 12:57:33 -0700

    Update guest users' IP addresses

--- lib/oregon_digital/guest_user.rb
+++ lib/oregon_digital/guest_user.rb
@@ -7,6 +7,12 @@ module OregonDigital
       u
     end
 
+    def guest_user
+      u = super
+      u.update_attributes(:current_sign_in_ip => request.remote_ip)
+      u
+    end
+
     def current_ability
       @current_ability ||= ::Ability.new(current_or_guest_user)
     end
