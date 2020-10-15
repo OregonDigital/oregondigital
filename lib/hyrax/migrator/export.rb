c0feebe43d9809abf465ae7b7072cd3de3ddc878
lsat12357 <lsato@uoregon.edu>
2020-10-15 08:07:19 -0700

    typo

--- lib/hyrax/migrator/export.rb
+++ lib/hyrax/migrator/export.rb
@@ -58,7 +58,7 @@ module Hyrax::Migrator
     end
 
     def add_content_to_keylist(item)
-      return if item.datastreams['content'].blank
+      return if item.datastreams['content'].blank?
 
       mimetype = asset_mimetype(item)
       return if !item.descMetadata.od_content.empty? && mimetype == 'xml'
