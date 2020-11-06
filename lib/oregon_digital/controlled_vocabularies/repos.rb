23a70970e4a7929a5cd6046d8551f95f24e292d2
Ryan Wick <ryan.wick@oregonstate.edu>
2020-11-05 22:24:19 -0800

    Add check for loc.gov URIs, change to https for fetch

--- lib/oregon_digital/controlled_vocabularies/repos.rb
+++ lib/oregon_digital/controlled_vocabularies/repos.rb
@@ -8,6 +8,20 @@ module OregonDigital::ControlledVocabularies
     use_vocabulary :ulan
     use_vocabulary :lcsh
 
+    # Make id.loc.gov URIs work for fetch, our gems don't handle redirect
+    # For fetch purposes only, change URI from http to https
+    def fetch
+      if self.rdf_subject.to_s.include?('loc.gov') then
+        self.rdf_subject.to_s.gsub!('http', 'https')
+      end
+
+      super
+
+      if self.rdf_subject.to_s.include?('loc.gov') then
+        self.rdf_subject.to_s.gsub!('https', 'http')
+      end
+    end
+
     class QaLcNames < Qa::Authorities::Loc
       include OregonDigital::Qa::Caching
       def search(q, sub_authority=nil)
