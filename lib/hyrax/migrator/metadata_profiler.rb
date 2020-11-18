893d1b4eef4c11402e1dd5f94621750fd5e4230b
lsat12357 <lsato@uoregon.edu>
2020-11-18 13:50:49 -0800

    streamline profiler

---
+++ lib/hyrax/migrator/metadata_profiler.rb
@@ -0,0 +1,117 @@
+
+module Hyrax::Migrator
+
+  module MetadataProfiler
+    INDENT = "  "
+    DASH = "- "
+
+    def create_profile(export_path, item)
+begin
+        f = File.open(File.join(export_path, "#{cleanpid(item.pid)}_profile.yml"), 'w')
+        f.puts "sets:"
+        f.print assemble_sets(item)
+        f.print assemble_primary(item)
+        f.print assemble_contents(item)
+        f.puts "derivatives_info:"
+        f.print assemble_derivatives_info(item.datastreams)
+        f.puts "fields:"
+        fields(item).each do |field|
+          vals = item.descMetadata.send(field)
+          next if vals.blank?
+
+          f.print assemble_field(field, vals)
+        end
+        f.close   
+      rescue StandardError => e
+        puts e.message
+        puts e.backtrace.join("\n")
+end
+    end
+
+    def assemble_derivatives_info(datastreams)
+      derivatives = external_datastreams(datastreams)
+      return if derivatives.empty?
+
+      str = "#{INDENT}has_thumbnail: #{derivatives.include? 'thumbnail'}\n"
+      str += "#{INDENT}has_content_ocr: #{derivatives.include? 'content_ocr'}\n"
+      str += "#{INDENT}page_count: #{derivatives.select { |d| d.start_with? 'page' }.count}\n"
+      str += "#{INDENT}has_content_ogg: #{derivatives.include? 'content_ogg'}\n"
+      str += "#{INDENT}has_content_mp3: #{derivatives.include? 'content_mp3'}\n"
+      str += "#{INDENT}has_medium_image: #{derivatives.include? 'medium'}\n"
+      str += "#{INDENT}has_pyramidal_image: #{derivatives.include? 'pyramidal'}\n"
+      str += "#{INDENT}has_content_mp4: #{derivatives.include? 'content_mp4'}\n"
+      str += "#{INDENT}has_content_jpg: #{derivatives.include? 'content_jpg'}\n"
+      str
+    end
+
+    def external_datastreams(input)
+      input.select { |_k, v| v.controlGroup == 'E' }.map { |k, _v| k }
+    end
+
+    def assemble_contents(item)
+      contents = query_graph(item, 'content')
+      return if contents.blank?
+
+      str = "contents:\n"
+      contents.each do |content|
+        str += "#{DASH}#{content}\n"
+      end
+      str
+    end
+
+    def assemble_sets(item)
+      str = "#{INDENT}set:\n"
+      query_graph(item, 'set').each do |set|
+        str += "#{INDENT}#{DASH}#{set}\n"
+      end
+      str
+    end
+
+    def assemble_primary(item)
+      str = "#{INDENT}primarySet: "
+      primary = query_graph(item, 'primarySet').first || ""
+      str + "#{primary}\n"
+    end
+
+    def assemble_field(field, vals)
+      str = "#{INDENT}#{field}:\n"
+      return str + "#{INDENT}#{field}: \"#{extract(vals)}\"\n" unless vals.is_a? Array
+
+      vals.each do |v|
+        str += "#{INDENT}#{DASH}\"#{extract(v)}\"\n"
+      end
+      str
+    end
+  
+    def query_graph(item, term)
+      pids = []
+      statements = item.descMetadata.graph.query(:predicate => RDF::URI("http://opaquenamespace.org/ns/#{term}"))
+      return pids if statements.empty?
+
+      statements.each do |s|
+        pids << s.object.to_s.split(':').last
+      end
+      pids
+    end
+
+    def fields(item)
+      item.resource.fields - [:primarySet, :set, :od_content]
+    end
+
+    def cleanpid(pid)
+      pid.gsub("oregondigital:", "")
+    end
+
+    def extract(val)
+      val.respond_to?(:rdf_subject) ? escape_chars(val.rdf_subject.to_s) : escape_chars(val.to_s)
+    end
+
+    def escape_chars(val)
+      val.gsub("\"", "\\\"")
+    end
+
+    def asset_mimetype(item)
+      item.datastreams['content'].mimeType.split('/').last
+    end
+  end
+end
---
+++ lib/tasks/migration/create_profiles.rake
@@ -0,0 +1,26 @@
+# For use as part of verification process in migrating assets from oregondigital
+# Requires a list of pids, one pid per line
+# to call: bundle exec rake create_profiles pids=/data1/batch/profiles/pidlist.txt export_path=/data1/batch/profiles
+
+desc 'Create asset profile yml file' 
+task create_profiles: :environment do
+require 'hyrax/migrator/metadata_profiler'
+  begin
+    export_path = ENV['export_path']
+    pidlist.each do |pid|
+      item = GenericAsset.find(pid)
+      Hyrax::Migrator::MetadataProfiler.create_profile(export_path, item)
+    end
+  rescue StandardError => e
+    puts e.message
+    puts e.backtrace.join("\n")
+  end
+end
+
+def pidlist
+  arr = []
+  File.readlines(ENV['pids']).each do |line|
+    arr << line.strip
+  end
+  arr
+end
