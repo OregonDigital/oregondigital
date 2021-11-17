363e0188c9ba50b033e49420c9366d2a32a093cb
lsat12357 <lsato@uoregon.edu>
2021-11-17 06:12:55 -0800

    reset errors

--- lib/hyrax/migrator/edtf_check.rb
+++ lib/hyrax/migrator/edtf_check.rb
@@ -5,11 +5,9 @@ module Hyrax::Migrator
   # check date fields for etdf formatting
   class EdtfCheck
     attr_accessor :work
-    def initialize
-      @errors = []
-    end
 
     def check_date_fields
+      reset
       edtf_fields.each do |field|
         @work.descMetadata.send(field).each do |value|
           @errors << "#{@work.pid}: in #{field}, #{value} is not in EDTF format" unless EDTF.parse(value).present?
@@ -20,6 +18,10 @@ module Hyrax::Migrator
 
     def edtf_fields
       %i[date acquisitionDate awardDate collectedDate issued viewDate]
+    end
+
+    def reset
+      @errors = []
     end
   end
 end
